155e8bbb996ca62f6b6729907d228dae2a9eab4d
Joey Chen <jay.hatelove@gmail.com>
2021-12-01 22:04:01 +0800

    [feat] no overlapping end < another.start

--- period.py
+++ period.py
@@ -6,6 +6,8 @@ class Period:
         self.start = start
 
     def get_overlapping_days(self, another):
+        if self.end < another.start:
+            return 0
         overlapping_start = self.start if self.start > another.start else another.start
         overlapping_end = self.end if self.end < another.end else another.end
         return (overlapping_end - overlapping_start).days + 1
