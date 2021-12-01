c424142db73aae7e8df73af97a6e9a02503cf827
Joey Chen <jay.hatelove@gmail.com>
2021-12-01 21:52:56 +0800

    [refactor] use another period for first_day and last_day

--- period.py
+++ period.py
@@ -7,8 +7,8 @@ class Period:
 
     def get_overlapping_days(self, budget):
         another = Period(budget.first_day(), budget.last_day())
-        first_day = budget.first_day()
-        last_day = budget.last_day()
+        first_day = another.start
+        last_day = another.end
         overlapping_start = self.start if self.start > first_day else first_day
         overlapping_end = self.end if self.end < last_day else last_day
         return (overlapping_end - overlapping_start).days + 1
